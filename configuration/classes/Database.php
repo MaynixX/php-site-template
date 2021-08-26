a12fe7994d9bfa0d0372af17e994675a14fa9ba6
MaynixX <78610918+MaynixX@users.noreply.github.com>
2021-08-26 18:37:51 +0300

    Update Database.php

--- configuration/classes/Database.php
+++ configuration/classes/Database.php
@@ -5,34 +5,34 @@ class Database{
 	    $this->link = mysqli_connect($host, $user, $password, $database); 
 		$this->link->set_charset("utf8mb4");
 	}
-
-	function select($query){
-		return mysqli_fetch_assoc(mysqli_query($this->link, $query));
+	public function query($query){
+		return $this->query($query);
+	}
+	
+	public function select($query){
+		return mysqli_fetch_assoc($this->query($query));
 	}
 
-	function query($query){
-		return mysqli_query($this->link, $query);
-	}
 
-	function fetch($query){
+	public function fetch($query){
 		$arr = [];
-		$result = mysqli_query($this->link, $query);
+		$result = $this->query($query);
 		while($item = mysqli_fetch_assoc($result)){
 			$arr[] = $item;
 		}
 		return $arr;
 	}
 
-	function numbering($query){
+	public function numbering($query){
 		$arr = [];
-		$result = mysqli_query($this->link, $query);
+		$result = $this->query($query);
 		while($item = mysqli_fetch_row($result)){
 			$arr[] = $item;
 		}
 		return $arr;
 	}
 
-	function count($query){
-        return mysqli_query($this->link, $query)->num_rows;
+	public function count($query){
+        	return $this->query($query)->num_rows;
 	}
 }
